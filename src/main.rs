a7d4d073f846255534e7953b4299aa49d2ede401
Ben Lipton <ben.lipton@gmail.com>
2019-06-25 04:53:26 -0400

    Very basic program structure and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::net::IpAddr;
+use std::io::Result;
+use std::net::{IpAddr, TcpStream, ToSocketAddrs};
+use std::process::exit;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -9,7 +11,29 @@ struct Opt {
     port: u16,
 }
 
+fn connect<A: ToSocketAddrs>(addr: A) -> Result<()> {
+    let stream = TcpStream::connect(addr)?;
+    Ok(())
+}
+
+fn listen<A: ToSocketAddrs>(addr: A) -> Result<()> {
+    Ok(())
+}
+
 fn main() {
     let opt = Opt::from_args();
-    println!("Hello, world! {:?}", opt);
+
+    let result = if opt.listen {
+        listen((opt.address, opt.port))
+    } else {
+        connect((opt.address, opt.port))
+    };
+
+    match result {
+        Err(_) => {
+            println!("Error");
+            exit(1);
+        }
+        _ => {}
+    }
 }
