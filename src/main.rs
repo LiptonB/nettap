e710ba2f344f61543b59b0363953c05753d1cbed
Ben Lipton <ben.lipton@gmail.com>
2019-06-25 18:11:52 -0400

    Argument parsing almost working, just need to wrap errors properly

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::io::Result;
-use std::net::{IpAddr, TcpStream, ToSocketAddrs};
+use std::net::{IpAddr, SocketAddr, TcpStream, ToSocketAddrs};
 use std::process::exit;
+use std::str::FromStr;
 use structopt::StructOpt;
 
+// TODO: address could be a hostname as well
 #[derive(Debug, StructOpt)]
 struct Opt {
     #[structopt(short = "l", long = "listen")]
     listen: bool,
-    address: IpAddr,
-    port: u16,
+    #[structopt(min_values = 1, max_values = 2)]
+    args: Vec<String>,
 }
 
 fn connect<A: ToSocketAddrs>(addr: A) -> Result<()> {
@@ -20,20 +22,37 @@ fn listen<A: ToSocketAddrs>(addr: A) -> Result<()> {
     Ok(())
 }
 
-fn main() {
+fn parse_options() -> Result<(bool, SocketAddr)> {
     let opt = Opt::from_args();
 
-    let result = if opt.listen {
-        listen((opt.address, opt.port))
+    let (address, port): (&str, &str) = if opt.listen {
+        match opt.args.as_slice() {
+            [port] => ("0.0.0.0", port),
+            [address, port] => (address, port),
+        }
     } else {
-        connect((opt.address, opt.port))
+        match opt.args.as_slice() {
+            [address, port] => (address, port),
+        }
     };
 
-    match result {
+    let address = IpAddr::from_str(address)?;
+    let port = u16::from_str(port)?;
+    return Ok((opt.listen, (address, port).into()));
+}
+
+fn main() {
+    let result = match parse_options() {
+        Ok((true, addr)) => listen(addr),
+        Ok((false, addr)) => connect(addr),
         Err(_) => {
-            println!("Error");
+            println!("Usage");
             exit(1);
         }
-        _ => {}
+    };
+
+    if let Err(_) = result {
+        println!("Error");
+        exit(2);
     }
 }
