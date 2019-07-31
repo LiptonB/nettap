7c4495c4ed07657f39157c5714ddc65831a68238
Ben Lipton <ben.lipton@gmail.com>
2019-07-31 09:39:00 -0400

    Switch to Bytes

--- src/connection.rs
+++ src/connection.rs
@@ -1,4 +1,4 @@
-use bytes::{Bytes, BytesMut};
+use bytes::Bytes;
 use failure::Error;
 use tokio::codec::{BytesCodec, Framed};
 use tokio::prelude::*;
@@ -9,14 +9,14 @@ pub fn tokio_connection<I, O, S>(
     socket: S,
 ) -> impl Future<Item = (), Error = ()>
 where
-    I: Sink<SinkItem = BytesMut, SinkError = Error>,
+    I: Sink<SinkItem = Bytes, SinkError = Error>,
     O: Stream<Item = Bytes, Error = Error>,
     S: AsyncRead + AsyncWrite,
 {
     let framed = Framed::new(socket, BytesCodec::new());
     let (socket_out, socket_in) = framed.split();
 
-    let in_fut = socket_in.from_err().forward(incoming);
+    let in_fut = socket_in.map(|b| b.freeze()).from_err().forward(incoming);
     let out_fut = outgoing.forward(socket_out);
     // TODO: Are we happy with this "swallow all errors" business
     in_fut.join(out_fut).map(|_| ()).map_err(|_| ())
