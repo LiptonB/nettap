1c298d3507a7393ab0b307b467bcafb38b676c46
Ben Lipton <ben.lipton@gmail.com>
2020-04-02 13:45:46 -0400

    Redo connection with async/await and error logging

--- src/connection.rs
+++ src/connection.rs
@@ -1,23 +1,53 @@
-use bytes::Bytes;
-use failure::Error;
+use bytes::{Bytes, BytesMut};
+use futures::stream::StreamExt;
+use futures::sink::{Sink, SinkExt};
 use tokio::prelude::*;
+use tokio::{join, stream::Stream, sync::mpsc};
 use tokio_util::codec::{BytesCodec, Framed};
+use log::error;
+use std::marker::Unpin;
 
-pub fn tokio_connection<I, O, S>(
-    incoming: I,
-    outgoing: O,
-    socket: S,
-) -> impl Future<Item = (), Error = ()>
+pub async fn tokio_connection<R, S>(sender: mpsc::Sender<Bytes>, receiver: R, socket: S)
 where
-    I: Sink<SinkItem = Bytes, SinkError = Error>,
-    O: Stream<Item = Bytes, Error = Error>,
-    S: AsyncRead + AsyncWrite,
+    R: Stream<Item = Bytes> + Unpin,
+    S: AsyncRead + AsyncWrite + Unpin,
 {
     let framed = Framed::new(socket, BytesCodec::new());
     let (socket_out, socket_in) = framed.split();
 
-    let in_fut = socket_in.map(|b| b.freeze()).from_err().forward(incoming);
-    let out_fut = outgoing.forward(socket_out);
-    // TODO: Are we happy with this "swallow all errors" business
-    in_fut.join(out_fut).map(|_| ()).map_err(|_| ())
+    let input_fut = stream_to_sender(socket_in, sender);
+    let output_fut = stream_to_sink(receiver, socket_out);
+    join!(input_fut, output_fut);
+}
+
+async fn stream_to_sender<S>(stream: S, sender: mpsc::Sender<Bytes>)
+where
+    S: Stream<Item = Result<BytesMut, std::io::Error>> + Unpin,
+{
+    while let Some(next) = stream.next().await {
+        match next {
+            Ok(bytes_mut) => {
+                if let Err(err) = sender.send(bytes_mut.freeze()).await {
+                    error!("Queue error: {}", err);
+                    break;
+                }
+            }
+            Err(err) => {
+                error!("Read error: {}", err);
+            }
+        }
+    }
+}
+
+async fn stream_to_sink<S, K>(stream: S, sink: K)
+where
+    S: Stream<Item = Bytes> + Unpin,
+    K: Sink<Bytes> + Unpin,
+    <K as Sink<Bytes>>::Error: std::fmt::Display,
+{
+    while let Some(bytes) = stream.next().await {
+        if let Err(err) = sink.send(bytes).await {
+            error!("Write error: {}", err);
+        }
+    }
 }
